f1c63931482279d225ba9ec1619336e795cbadf3
Duane Michael <dmichaeljr@pm.me>
2022-02-19 12:24:54 -0700

    Manually merged initial DPAPI code

---
+++ lib/Dpapi.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+
+namespace SharpSCCM
+{
+    public class Dpapi
+    {
+        public static void Execute(string blob)
+        {
+            Console.WriteLine("\r\n[*] Action: Describe DPAPI blob");
+
+            // 1. Read in the hex dpapi blob
+            // 2. Convert it to bytes
+            // 3. Trim the extra header
+
+            //$bytes = for($i=0; $i -lt $bdpass.Length; $i++) {[byte]::Parse($bdpass.Substring($i, 2), [System.Globalization.NumberStyles]::HexNumber); $i++}
+            byte[] blobBytes = new byte[blob.Length];
+            for (int i = 0; i < blob.Length; i++)
+            {
+                blobBytes[i] = Byte.Parse(blob.Substring(i, 2), System.Globalization.NumberStyles.HexNumber);
+                Console.WriteLine(blobBytes[i]);
+                i++;
+            }
+
+            // NAA has a header larger than a normal DPAPI blob. Remove the first 4 bytes from the array.
+            var offset = 4;
+            var dedupedArray = new byte[blob.Length - offset];
+            Buffer.BlockCopy(blobBytes, 4, dedupedArray, 0, blobBytes.Length - offset);
+            //System.Convert.ToBase64String(dedupedArray);
+
+            if (blobBytes.Length > 0)
+            {
+                byte[] decBytesRaw = DescribeDPAPIBlob(blobBytes, masterkeys, "blob", unprotect, entropy);
+                
+                if ((decBytesRaw != null) && (decBytesRaw.Length != 0))
+                {
+                    if (Helpers.IsUnicode(decBytesRaw))
+                    {
+                        string data = "";
+                        int finalIndex = Array.LastIndexOf(decBytesRaw, (byte)0);
+                        if (finalIndex > 1)
+                        {
+                            byte[] decBytes = new byte[finalIndex + 1];
+                            Array.Copy(decBytesRaw, 0, decBytes, finalIndex);
+                            data = Encoding.Unicode.GetString(decBytes);
+                        }
+                        else
+                        {
+                            data = Encoding.ASCII.GetString(decBytesRaw);
+                        }
+                        Console.WriteLine("   dec(blob)     : {0}", data);
+                    }
+                    else
+                    {
+                        string hexData = BitConverter.ToString(decBytesRaw).Replace("-", " ");
+                        Console.WriteLine("   dec(blob)     : {0}", hexData);
+                    }
+                }
+            }
+        }
+
+        public static byte[] DescribeDPAPIBlob(byte[] blobBytes, Dictionary<string, string> MasterKeys)
+        {
+            // Parses a DPAPI blob returning the decrypted plaintext
+
+            var offset = 24; // Set to 24 since we're only working with 'blob' blobType
+
+            return new byte[0]; //temp
+
+        }
+
+        public static byte[] DecryptBlob(byte [] ciphertext, byte[] key, int algCrypt, PaddingMode padding = PaddingMode.Zeros)
+        {
+            // decrypts a DPAPI blob using AES
+
+            // takes a byte array of ciphertext bytes and a key array, decrypt the blob with AES256
+            var aesCryptoProvider = new AesManaged();
+            var ivBytes = new byte[16];
+            aesCryptoProvider.Key = key;
+            aesCryptoProvider.IV = ivBytes;
+            aesCryptoProvider.Mode = CipherMode.CBC;
+            aesCryptoProvider.Padding = padding;
+
+            var plaintextBytes = aesCryptoProvider.CreateDecryptor().TransformFinalBlock(ciphertext, 0, ciphertext.Length);
+
+            return plaintextBytes;
+        }
+    }
+}
---
+++ lib/Interop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpSCCM
+{
+    public class Interop
+    {
+        // For unicode detection
+        [Flags]
+        public enum IsTextUnicodeFlags : int
+        {
+            IS_TEXT_UNICODE_ASCII16 = 0x0001,
+            IS_TEXT_UNICODE_REVERSE_ASCII16 = 0x0010,
+
+            IS_TEXT_UNICODE_STATISTICS = 0x0002,
+            IS_TEXT_UNICODE_REVERSE_STATISTICS = 0x0020,
+
+            IS_TEXT_UNICODE_CONTROLS = 0x0004,
+            IS_TEXT_UNICODE_REVERSE_CONTROLS = 0x0040,
+
+            IS_TEXT_UNICODE_SIGNATURE = 0x0008,
+            IS_TEXT_UNICODE_REVERSE_SIGNATURE = 0x0080,
+
+            IS_TEXT_UNICODE_ILLEGAL_CHARS = 0x0100,
+            IS_TEXT_UNICODE_ODD_LENGTH = 0x0200,
+            IS_TEXT_UNICODE_DBCS_LEADBYTE = 0x0400,
+            IS_TEXT_UNICODE_NULL_BYTES = 0x1000,
+
+            IS_TEXT_UNICODE_UNICODE_MASK = 0x000F,
+            IS_TEXT_UNICODE_REVERSE_MASK = 0x00F0,
+            IS_TEXT_UNICODE_NOT_UNICODE_MASK = 0x0F00,
+            IS_TEXT_UNICODE_NOT_ASCII_MASK = 0xF000
+        }
+
+        [DllImport("advapi.dll", CharSet = CharSet.Auto)]
+        public static extern bool IsTextUnicode
+        (
+            byte[] buf,
+            int len,
+            ref IsTextUnicodeFlags opt
+        );
+    }
+}
