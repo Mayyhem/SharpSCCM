42d1cd1d11d61c8c49359089ffd3a737a0d486bd
Duane Michael <41414134+subat0mik@users.noreply.github.com>
2022-04-12 17:38:04 -0600

    Removed unfinished credential function.

--- lib/Credentials.cs
+++ lib/Credentials.cs
@@ -46,36 +46,5 @@ namespace SharpSCCM
                 Console.WriteLine($"[+] Found 0 instances of CCM_NetworkAccessAccount");
             }
         }
-
-        // -------------------------------------------------------------------------------------------------------------------------------
-        // https://gist.github.com/EvanMcBroom/525d84b86f99c7a4eeb4e3495cffcbf0
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct GarbledData
-        {
-            UInt32 dwVersion;
-            Byte[] key;
-            THeaderInfo header;
-            Byte[] pData;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct THeaderInfo
-        {
-            UInt32 nHeaderLength; // Must be 0x14
-            UInt32 nEncryptedSize;
-            UInt32 nPlainSize;
-            UInt32 nAlgorithm;
-            UInt32 nFlag;
-        }
-        public static void QueryDecryptNetworkAccessAccount()
-        {
-            /*
-            * Research by Evan McBroom and Chris Thompson (@_Mayyhem)
-            * Roger Zander made security recommendations for SCCM based on the claim that NAA credentials could be recovered.
-            * Source: https://rzander.azurewebsites.net/network-access-accounts-are-evil/
-            * Roger stated that recovery was "possible with a few lines of code" but did not provide any code. Here is working code.
-            */
-        }
     }
 }
