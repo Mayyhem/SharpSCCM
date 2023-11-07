f873b56f62856f7e374937d27f0671f97789b0eb
Mayyhem <30671833+Mayyhem@users.noreply.github.com>
2023-11-07 13:26:00 -0500

    Updated version info and release notes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.2")]
-[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3")]
+[assembly: AssemblyFileVersion("2.0.3")]
